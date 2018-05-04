afd005f79f3011b96921a53a21a60655281dfb33
bahartmann <barbarahartma@gmail.com>
2018-05-03 21:10:50 -0300

    Do not force env file existence

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -3,7 +3,7 @@ module EnvironmentVariables
     config.before_configuration do
       env_file = Rails.root.join("config", 'environment_variables.yml').to_s
 
-      if File.exists?(env_file)
+      if File.exists?(env_file) && YAML.load_file(env_file)[Rails.env]
         YAML.load_file(env_file)[Rails.env].each do |key, value|
           ENV[key] ||= value.to_s
         end
